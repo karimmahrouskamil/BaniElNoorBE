50ee037f71e7674f08945a826930a464d18ed6d1
karim kamil <kkamil@eg.ibm.com>
2019-07-06 17:45:39 +0200

    mysql added

---
+++ config/database-config.js
@@ -0,0 +1,22 @@
+// var mysql = require("mysql");
+
+// /**
+//  * database connection
+//  */
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "",
+//   password: "",
+//   database: "test",
+//   port: 20954
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.log("ERROR" + err);
+//   } else {
+//     console.log("CONNECTED");
+//   }
+// });
+// module.exports = connection;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+// var db = require('../config/database-config');
 /* GET home page. */
 router.get('/', function(req, res, next) {
    res.render('index');
 });
 router.get('/test', function(req, res, next) {
    res.json({'test':'working'});
+   
 });
 
 module.exports = router;
