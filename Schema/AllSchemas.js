3e67975a607a7bb0729afe50f933dbf76f1ca86f
karim kamil <kkamil@eg.ibm.com>
2019-08-03 15:12:49 +0200

    All schemas object

---
+++ Schema/AllSchemas.js
@@ -0,0 +1,17 @@
+var CLientSchema = require("./ClientSchema");
+var ServantSchema = require("./ServantSchema");
+var PriestSchema = require("./PriestSchema");
+var CategorytSchema = require("./CategoriesSchema");
+var ZoneSchema = require("./ZoneSchema");
+var ClassSchema = require("./ClassSchema");
+var SchoolSchema = require("./SchoolSchema");
+
+module.exports.All_SCHEMAS = {
+  ClientSchema: CLientSchema,
+  ServantSchema: ServantSchema,
+  PriestSchema: PriestSchema,
+  CategorytSchema: CategorytSchema,
+  ZoneSchema: ZoneSchema,
+  ClassSchema: ClassSchema,
+  SchoolSchema: SchoolSchema
+};
--- Services/clientService.js
+++ Services/clientService.js
@@ -1,41 +1,36 @@
 var Promise = require("promise");
 
-// schemaas 
-var CLientSchema = require("../Schema/ClientSchema");
-var ServantSchema = require("../Schema/ServantSchema");
-var PriestSchema = require("../Schema/PriestSchema");
-var CategorytSchema = require("../Schema/CategoriesSchema");
-var ZoneSchema = require("../Schema/ZoneSchema");
-var ClassSchema = require("../Schema/ClassSchema");
-var SchoolSchema = require("../Schema/SchoolSchema");
-
-// connection to db 
+// schemaas
+var AllSchemas = require("../Schema/AllSchemas");
+// connection to db
 var Connection = require("../config/database-config");
 
 module.exports.getAllClients = function() {
   return new Promise(function(resolve, reject) {
     Connection.query(
       "SELECT * FROM ?? as CL inner join ?? as PR  on CL." +
-        CLientSchema.CLIENT_TABLE.ClientsFatherOfConfession +
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE
+          .ClientsFatherOfConfession +
         " = PR.ID inner join ?? as SE on CL." +
-        CLientSchema.CLIENT_TABLE.ClientServantFollowing +
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE
+          .ClientServantFollowing +
         " = SE.ID inner join ?? as Ca  on CL." +
-        CLientSchema.CLIENT_TABLE.clientCategoryID +
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE.clientCategoryID +
         " = Ca.ID inner join ?? as Zo on CL." +
-        CLientSchema.CLIENT_TABLE.ClientZone +
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE.ClientZone +
         " = Zo.ID inner join ?? as CLA on CL." +
-        CLientSchema.CLIENT_TABLE.ClientClass +
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE.ClientClass +
         " = CLA.ID inner join ?? as SCH on CL." +
-        CLientSchema.CLIENT_TABLE.ClientSchoolID +
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE.ClientSchoolID +
         " = SCH.ID",
       [
-        CLientSchema.CLIENT_TABLE.TableName,
-        PriestSchema.PRIEST_TABLE.TableName,
-        ServantSchema.SERVANT_TABLE.TableName,
-        CategorytSchema.CATEGRORIES_TABLE.TableName,
-        ZoneSchema.ZONE_TABLE.TableName,
-        ClassSchema.CLASS_TABLE.TableName,
-        SchoolSchema.SCHOOL_TABLE.TableName
+        AllSchemas.All_SCHEMAS.ClientSchema.CLIENT_TABLE.TableName,
+        AllSchemas.All_SCHEMAS.PriestSchema.PRIEST_TABLE.TableName,
+        AllSchemas.All_SCHEMAS.ServantSchema.SERVANT_TABLE.TableName,
+        AllSchemas.All_SCHEMAS.CategorytSchema.CATEGRORIES_TABLE.TableName,
+        AllSchemas.All_SCHEMAS.ZoneSchema.ZONE_TABLE.TableName,
+        AllSchemas.All_SCHEMAS.ClassSchema.CLASS_TABLE.TableName,
+        AllSchemas.All_SCHEMAS.SchoolSchema.SCHOOL_TABLE.TableName
       ],
       function(error, results, fields) {
         if (error) {
